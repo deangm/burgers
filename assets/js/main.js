f53509b0e2beca976365fe7db6b7e554b9de444f
mikey <michael.d.deangelo@Gmail.com>
2019-09-09 11:13:02 -0600

    stops transitions from occuring on page load....adds a class of preload to body and removes it with JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,5 @@
-function showMenu(){
-	chicken = document.getElementById('chicken');
-	thing = document.getElementById('thing');
-	
-	chicken.onclick = function(){
-		thing.setAttribute("style", "visibility:visible")
-	}
-}
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-	showMenu();
-});
+$(window).load(function() {
+	$("body").removeClass("preload");
+  });
 
 
