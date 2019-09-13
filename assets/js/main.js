e232db38bb42cc901ea250f11fec4900e9a0e09c
mikey <michael.d.deangelo@Gmail.com>
2019-09-13 15:22:55 -0600

    adds opacity onclick

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,10 @@ $(window).load(function() {
 
 function modalVisible(){
   document.getElementById("modal").style.visibility=("visible");
-  document.getElementsByClassName("container").style.opacity(".5");
+  document.getElementById("container").style.opacity=(".5");
 }
 
 function closeModal(){
   document.getElementById("modal").style.visibility=("hidden");
+  document.getElementById("container").style.opacity=("1");
 }
